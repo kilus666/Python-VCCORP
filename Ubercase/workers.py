bb6bb07ccebf5a3aea5ea20fc09cbdc5aec76c21
Corgi Learn to code <mrkevin2610@gmail.com>
2019-07-10 12:00:24 +0700

    Uber analysis update

--- Ubercase/workers.py
+++ Ubercase/workers.py
@@ -4,3 +4,4 @@ def read_csv_to_datetime(file):
     monthly_data= pd.read_csv(file,delimiter=",",header=0)
     monthly_data["Date/Time"]=pd.to_datetime(monthly_data["Date/Time"])
     return monthly_data
+
